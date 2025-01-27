91d288aa257fb97e88e80b2a6cafb4b81ab45458
eunbyul <eunbyul.ko@nflux.co.kr>
2025-01-28 02:21:37 +0900

    article 조회 페이지 생성

--- src/test/kotlin/com/example/blog/IntegrationTests.kt
+++ src/test/kotlin/com/example/blog/IntegrationTests.kt
@@ -1,4 +1,5 @@
 import com.example.blog.BlogApplication
+import com.example.blog.extention.toSlug
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
@@ -24,12 +25,16 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
         println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>", "Lorem")
     }
 
     @Test
     fun `Assert article page title, content and status code`() {
-        println(">> TODO")
+        println(">> Assert article page title, content and status code")
+        val title = "Lorem"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(title, "Lorem", "dolor sit amet")
     }
 
     @AfterAll
